8ffa6aaa4d6b22c5e0280510cce00db6e0b67d89
LiuBaoYing <92360139+LBY89@users.noreply.github.com>
2022-04-01 10:37:24 +0300

    Update Statistics.js

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,13 +1,9 @@
-import React, { useState } from 'react'
-const { useMemo } = React
-const useCountClicks = allClicks =>
-  useMemo(() => allClicks.reduce((acc, x) => ({
-    ...acc,
-    [x]: (acc[x] || 0) + 1
-  }), { all: allClicks.length }), [allClicks])
+import React from 'react'
 
-const Statistics = ({ allClicks }) => {
-  const { all, G, N, B } = useCountClicks(allClicks)
+
+const Statistics = ({ allClicks, good, bad, neutral}) => {
+  const all = allClicks.length
+
   if (all === 0) {
     return (
       <div>
@@ -21,17 +17,17 @@ const Statistics = ({ allClicks }) => {
         <tbody style={{width: '30%', padding: '5px'}}>
             <tr>
               <td>good</td>
-              <td>{G}</td>
+              <td>{good}</td>
             </tr>
 
             <tr>
               <td>neutral</td>
-              <td>{N}</td>
+              <td>{neutral}</td>
             </tr>
           
             <tr>
               <td>bad</td> 
-              <td>{B}</td>
+              <td>{bad}</td>
             </tr>
           
             <tr><td>all</td>
@@ -40,12 +36,12 @@ const Statistics = ({ allClicks }) => {
           
             <tr>
               <td>average</td>
-              <td>{((G-B)/all).toFixed(1)}</td>
+              <td>{((good-bad)/all).toFixed(1)}</td>
             </tr>
           
             <tr>
               <td>positive</td> 
-              <td>{(100*(all - B)/(all)).toFixed(1)} %</td>
+              <td>{(100*(all - bad)/(all)).toFixed(1)} %</td>
             </tr>
         </tbody>
      </table>
